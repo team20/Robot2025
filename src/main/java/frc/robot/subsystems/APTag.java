ef1e1b8fa08f6477b27ebe18eb59577dc5363c21
Vincent-Christianson <155695107+Vincent-Christianson@users.noreply.github.com>
2025-01-11 13:10:56 -0500

    update

---
+++ src/main/java/frc/robot/subsystems/APTag.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+
+public class APTag {
+	PhotonCamera camera;
+
+	public APTag() {
+		camera = new PhotonCamera("Cool Camera");
+	}
+
+	boolean getHasTarget() {
+		PhotonPipelineResult result = camera.getAllUnreadResults().get(0);
+
+		return result.hasTargets();
+	}
+}
