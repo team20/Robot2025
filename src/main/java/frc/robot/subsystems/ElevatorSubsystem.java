d18354e291cf8863bee7278be4a9e070b111267b
Natalie Mann <117381803+NatalieEMann@users.noreply.github.com>
2025-01-12 12:37:04 -0500

    Convert to Master & Follower + Add of PID Controller

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -7,8 +7,10 @@ package frc.robot.subsystems;
 import static frc.robot.Constants.ElevatorConstants.*;
 
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
@@ -30,21 +32,20 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ElevatorSubsystem extends SubsystemBase {
 
-	private final SparkMax m_rightMotor = new SparkMax(kElevatorMotorRightPort, MotorType.kBrushless); // Master
-	private final SparkMax m_leftMotor = new SparkMax(kElevatorMotorLeftPort, MotorType.kBrushless); // Follower
+	private final SparkMax m_masterMotor = new SparkMax(kElevatorMotorRightPort, MotorType.kBrushless);
+	private final SparkMax m_followerMotor = new SparkMax(kElevatorMotorLeftPort, MotorType.kBrushless);
 
-	private final RelativeEncoder m_leftEncoder = m_leftMotor.getEncoder(); // TODO: Is this the right encoder?
+	private final RelativeEncoder m_masterEncoder = m_masterMotor.getEncoder(); // TODO: Is this the right encoder?
 
-	private final RelativeEncoder m_rightEncoder = m_rightMotor.getEncoder();
+	private final SparkClosedLoopController m_closedLoopController = m_masterMotor.getClosedLoopController();
 
 	private double m_setPositionLeft = 0;
-	private double m_setPositionRight = 0;
 
 	/** Creates a new ElevatorSubsystem. */
 	public ElevatorSubsystem() {
-		SparkMaxConfig config = new SparkMaxConfig();
+		var config = new SparkMaxConfig();
 		config
-				.inverted(false) // TODO: may need to change if one motor is inversed and the other is not
+				.inverted(false)
 				.idleMode(IdleMode.kBrake) // TODO: Soft limits?
 				.smartCurrentLimit(kSmartCurrentLimit)
 				.secondaryCurrentLimit(kSecondaryCurrentLimit)
@@ -53,8 +54,13 @@ public class ElevatorSubsystem extends SubsystemBase {
 				.feedbackSensor(FeedbackSensor.kPrimaryEncoder)
 				.pid(kP, kI, kD);
 
-		m_rightMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-		m_leftMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+		var followerConfig = new SparkMaxConfig();
+		followerConfig.apply(config);
+		followerConfig
+				.follow(kElevatorMotorRightPort); // TODO: INVERTED?
+
+		m_masterMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+		m_followerMotor.configure(followerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
 		resetEncoder();
 	}
@@ -65,8 +71,76 @@ public class ElevatorSubsystem extends SubsystemBase {
 	 * @param speed the speed you want to set it to
 	 */
 	public void setSpeed(double speed) {
-		m_rightMotor.set(speed);
-		m_leftMotor.set(speed);
+		m_masterMotor.set(speed);
+	}
+
+	/**
+	 * Gets the position of the left motor
+	 * 
+	 * @return the position of the left motor (through the encoder value)
+	 */
+	public double getleftPosition() {
+		return m_masterEncoder.getPosition();
+	}
+
+	/**
+	 * Tests if within the tolerance of the setpoint of the left motor
+	 * 
+	 * @return true if at the setpoint
+	 */
+	public boolean atleftSetpoint() {
+		return (Math.abs(m_setPositionLeft - getleftPosition()) <= kTolerance);
+	}
+
+	/**
+	 * Sets the position of the motors (using PID)
+	 * 
+	 * @param position Position to set the master motor to (the follower
+	 *        will...follow)
+	 */
+	public void setPosition(double position) {
+		m_setPositionLeft = position;
+		m_closedLoopController.setReference(position, ControlType.kPosition);
+	}
+
+	/**
+	 * Resets the encoder value to 0 (starting position)
+	 */
+	public void resetEncoder() {
+		m_masterEncoder.setPosition(0);
+		setPosition(0);
+	}
+
+	/**
+	 * Determine the output current of the motors
+	 * 
+	 * @return The max value that the output current gets to
+	 */
+	public double getOutputCurrent() {
+		return Math.abs(m_masterMotor.getOutputCurrent()); // TODO: Needs other motor?
+	}
+
+	/**
+	 * Checks if position is at 0 (and within tolerance)
+	 * 
+	 * @return true if within tolerance
+	 */
+	public boolean atBase() {
+		return (Math.abs(getleftPosition()) <= kTolerance);
+	}
+
+	@Override
+	public void periodic() {
+		// This method will be called once per scheduler run
+	}
+
+	/**
+	 * Stops the motor
+	 * 
+	 * @return Sets the speed to 0
+	 */
+	public Command stopMotor() {
+		return runOnce(() -> setSpeed(0));
 	}
 
 	/**
@@ -87,91 +161,6 @@ public class ElevatorSubsystem extends SubsystemBase {
 		return runOnce(() -> setSpeed(1));
 	}
 
-	/**
-	 * Stops the motor
-	 * 
-	 * @return Sets the speed to 0
-	 */
-	public Command stopMotor() {
-		return runOnce(() -> setSpeed(0));
-	}
-
-	/**
-	 * Gets the position of the left motor
-	 * 
-	 * @return the position of the left motor (through the encoder value)
-	 */
-	public double getleftPosition() {
-		return m_leftEncoder.getPosition();
-	}
-
-	/**
-	 * Gets the position of the right motor
-	 * 
-	 * @return the position of the right motor (through the encoder value)
-	 */
-	public double getrightPosition() {
-		return m_rightEncoder.getPosition();
-	}
-
-	/**
-	 * Tests if within the tolerance of the setpoint of the left motor
-	 * 
-	 * @return true if at the setpoint
-	 */
-	public boolean atleftSetpoint() {
-		return (Math.abs(m_setPositionLeft - getleftPosition()) <= kTolerance);
-	}
-
-	/**
-	 * Tests if within the tolerance of the setpoint of the right motor
-	 * 
-	 * @return true if at the setpoint
-	 */
-	public boolean atrightSetpoint() {
-		return (Math.abs(m_setPositionRight - getrightPosition()) <= kTolerance);
-	}
-
-	/**
-	 * Sets the position of the motors (using PID)
-	 * 
-	 * @param positionLeft Position to set the left motor to
-	 * @param positionRight Position to set the right motor to
-	 */
-	public void setPosition(double position) {
-		m_setPositionLeft = position;
-		// m_leftPidController.setReference(positionLeft, ControlType.kPosition);
-		// m_rightPidController.setReference(positionRight, ControlType.kPosition);
-	}
-
-	/**
-	 * Resets the encoder value to 0 (starting position)
-	 */
-	public void resetEncoder() {
-		m_leftEncoder.setPosition(0);
-		m_rightEncoder.setPosition(0);
-		setPosition(0);
-	}
-
-	/**
-	 * Determine the output current of the motors
-	 * 
-	 * @return The max value that the output current gets to
-	 */
-	public double getOutputCurrent() {
-		return Math.max(Math.abs(m_leftMotor.getOutputCurrent()), Math.abs(m_rightMotor.getOutputCurrent()));
-	}
-
-	/**
-	 * Checks if position is at 0 (and within tolerance)
-	 * 
-	 * @return true if within tolerance
-	 */
-	public boolean atBase() {
-		return ((Math.abs(getrightPosition()) <= kTolerance)
-				&& (Math.abs(getleftPosition()) <= kTolerance));
-	}
-
 	public Command setPositionBase() {
 		return runOnce(() -> setPosition(0));
 	}
@@ -182,11 +171,6 @@ public class ElevatorSubsystem extends SubsystemBase {
 
 	public Command setPositionTop() {
 		return runOnce(() -> setPosition(kMaxExtension));
-	}
-
-	@Override
-	public void periodic() {
-		// This method will be called once per scheduler run
 	}
 
 }
