24111deeae01edf066bef09d731ff203f12b6ee8
J Hwang <100035934+jhhbrown1@users.noreply.github.com>
2025-01-20 17:09:59 -0500

    added simulation to PoseEstimationSubsystem

--- src/main/java/frc/robot/subsystems/PoseEstimationSubsystem.java
+++ src/main/java/frc/robot/subsystems/PoseEstimationSubsystem.java
@@ -6,6 +6,8 @@ import static frc.robot.Constants.RobotConstants.*;
 import java.util.Optional;
 
 import org.photonvision.PhotonCamera;
+import org.photonvision.simulation.PhotonCameraSim;
+import org.photonvision.simulation.VisionSystemSim;
 import org.photonvision.targeting.PhotonPipelineResult;
 
 import edu.wpi.first.math.VecBuilder;
@@ -16,6 +18,9 @@ import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructPublisher;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -56,6 +61,22 @@ public class PoseEstimationSubsystem extends SubsystemBase {
 	private double m_previousTimestamp = 0;
 
 	/**
+	 * The {@code PhotonCameraSim} used by this {@code PoseEstimationSubsystem}.
+	 */
+	private PhotonCameraSim m_cameraSim = null;
+
+	/**
+	 * The {@code VisionSystemSim} used by this {@code PoseEstimationSubsystem}.
+	 */
+	private VisionSystemSim m_sim = null;
+
+	/**
+	 * The {@code StructPublisher} for reporting the estimated {@code Pose2d} of the
+	 * robot.
+	 */
+	private final StructPublisher<Pose2d> m_posePublisher;
+
+	/**
 	 * Constructs a {@code PoseEstimationSubsystem}.
 	 * 
 	 * @param cameraName the nickname of the camera to be used by the
@@ -73,6 +94,20 @@ public class PoseEstimationSubsystem extends SubsystemBase {
 				new Pose2d(),
 				stateStdDevs,
 				visionMeasurementStdDevs);
+		if (RobotBase.isSimulation()) {
+			m_sim = new VisionSystemSim("sim");
+			m_sim.addAprilTags(kFieldLayout);
+			m_cameraSim = new PhotonCameraSim(m_camera);
+			m_sim.addCamera(m_cameraSim, kRobotToCamera);
+			m_cameraSim.enableProcessedStream(true);
+			m_cameraSim.enableDrawWireframe(true);
+		} else {
+			m_sim = null;
+			m_cameraSim = null;
+		}
+		m_posePublisher = NetworkTableInstance.getDefault()
+				.getStructTopic("/SmartDashboard/Pose@PoseEstimationSubsystem", Pose2d.struct)
+				.publish();
 	}
 
 	/**
@@ -96,6 +131,16 @@ public class PoseEstimationSubsystem extends SubsystemBase {
 			}
 		}
 		m_poseEstimator.update(m_driveSubsystem.getHeading(), m_driveSubsystem.getModulePositions());
+		m_posePublisher.set(m_poseEstimator.getEstimatedPosition());
+	}
+
+	/**
+	 * Is invoked periodically by the {@link CommandScheduler}. Useful for updating
+	 * subsystem-specific state that needs to be maintained for simulations.
+	 */
+	@Override
+	public void simulationPeriodic() {
+		m_sim.update(m_driveSubsystem.getPose());
 	}
 
 	/**
@@ -106,5 +151,4 @@ public class PoseEstimationSubsystem extends SubsystemBase {
 	public Pose2d getPose() {
 		return m_poseEstimator.getEstimatedPosition();
 	}
-
 }
