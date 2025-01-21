46cec6a880123fd3117830f062c3e70bbe334bbf
J Hwang <100035934+jhhbrown1@users.noreply.github.com>
2025-01-21 05:48:57 -0500

    PhotonCameraSimulator: more realistic, PoseEstimationSubsystem: uses both PhotonPoseEstimator and SwerveDrivePoseEstimator, PhotonVisionSubsystem: no longer needed

--- src/main/java/frc/robot/subsystems/PoseEstimationSubsystem.java
+++ src/main/java/frc/robot/subsystems/PoseEstimationSubsystem.java
@@ -3,17 +3,15 @@ package frc.robot.subsystems;
 import static frc.robot.Constants.*;
 import static frc.robot.Constants.RobotConstants.*;
 
-import java.util.Optional;
-
 import org.photonvision.PhotonCamera;
-import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -47,22 +45,27 @@ public class PoseEstimationSubsystem extends SubsystemBase {
 	private static final Vector<N3> visionMeasurementStdDevs = VecBuilder.fill(0.5, 0.5, Units.degreesToRadians(10));
 
 	/**
+	 * The {@code PhotonPoseEstimator} used by this {@code PoseEstimationSubsystem}.
+	 */
+	private final PhotonPoseEstimator m_photonPoseEstimator;
+
+	/**
 	 * The {@code SwerveDrivePoseEstimator} used by this
 	 * {@code PoseEstimationSubsystem}.
 	 */
 	private final SwerveDrivePoseEstimator m_poseEstimator;
 
 	/**
-	 * The timestamp of the latest {@code PhotonPipelineResult} received from the
-	 * {@code PhotonCamera}.
+	 * The {@code StructPublisher} for reporting the detected {@code Pose2d} of the
+	 * robot.
 	 */
-	private double m_previousTimestamp = 0;
+	private final StructPublisher<Pose2d> m_detectedPosePublisher;
 
 	/**
 	 * The {@code StructPublisher} for reporting the estimated {@code Pose2d} of the
 	 * robot.
 	 */
-	private final StructPublisher<Pose2d> m_posePublisher;
+	private final StructPublisher<Pose2d> m_estimatedPosePublisher;
 
 	/**
 	 * Constructs a {@code PoseEstimationSubsystem}.
@@ -73,9 +76,14 @@ public class PoseEstimationSubsystem extends SubsystemBase {
 	 *        {@code PoseEstimationSubsystem}
 	 */
 	public PoseEstimationSubsystem(String cameraName, DriveSubsystem driveSubsystem) {
-		m_camera = RobotBase.isSimulation() ? new PhotonCameraSimulator(cameraName, driveSubsystem, 3, 0.1)
+		m_camera = RobotBase.isSimulation()
+				? new PhotonCameraSimulator(cameraName, driveSubsystem, new Pose2d(1, 1, Rotation2d.fromDegrees(0)),
+						0.01, 3,
+						0.1)
 				: new PhotonCamera(cameraName);
 		this.m_driveSubsystem = driveSubsystem;
+		m_photonPoseEstimator = new PhotonPoseEstimator(kFieldLayout,
+				PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, kRobotToCamera);
 		m_poseEstimator = new SwerveDrivePoseEstimator(
 				driveSubsystem.kinematics(),
 				driveSubsystem.getHeading(),
@@ -83,7 +91,10 @@ public class PoseEstimationSubsystem extends SubsystemBase {
 				new Pose2d(),
 				stateStdDevs,
 				visionMeasurementStdDevs);
-		m_posePublisher = NetworkTableInstance.getDefault()
+		m_detectedPosePublisher = NetworkTableInstance.getDefault()
+				.getStructTopic("/SmartDashboard/Pose@PhotonCamera", Pose2d.struct)
+				.publish();
+		m_estimatedPosePublisher = NetworkTableInstance.getDefault()
 				.getStructTopic("/SmartDashboard/Pose@PoseEstimationSubsystem", Pose2d.struct)
 				.publish();
 	}
@@ -93,23 +104,16 @@ public class PoseEstimationSubsystem extends SubsystemBase {
 	 */
 	@Override
 	public void periodic() {
-		PhotonPipelineResult pipelineResult = m_camera.getLatestResult();
-		var timestamp = pipelineResult.getTimestampSeconds();
-		if (timestamp != m_previousTimestamp && pipelineResult.hasTargets()) {
-			m_previousTimestamp = timestamp;
-			var target = pipelineResult.getBestTarget();
-			var fiducialId = target.getFiducialId();
-			Optional<Pose3d> tagPose = kFieldLayout == null ? Optional.empty()
-					: kFieldLayout.getTagPose(fiducialId);
-			if (target.getPoseAmbiguity() <= .2 && fiducialId >= 0 && tagPose.isPresent()) {
-				Transform3d camToTarget = target.getBestCameraToTarget();
-				Pose3d camPose = tagPose.get().transformBy(camToTarget.inverse());
-				var visionMeasurement = camPose.transformBy(kRobotToCamera.inverse());
-				m_poseEstimator.addVisionMeasurement(visionMeasurement.toPose2d(), timestamp);
+		for (var r : m_camera.getAllUnreadResults()) {
+			var e = m_photonPoseEstimator.update(r);
+			if (e.isPresent()) {
+				var v = e.get();
+				m_poseEstimator.addVisionMeasurement(v.estimatedPose.toPose2d(), v.timestampSeconds);
+				m_detectedPosePublisher.set(v.estimatedPose.toPose2d());
 			}
 		}
 		m_poseEstimator.update(m_driveSubsystem.getHeading(), m_driveSubsystem.getModulePositions());
-		m_posePublisher.set(m_poseEstimator.getEstimatedPosition());
+		m_estimatedPosePublisher.set(m_poseEstimator.getEstimatedPosition());
 	}
 
 	/**
