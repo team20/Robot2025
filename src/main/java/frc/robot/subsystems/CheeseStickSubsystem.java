3c76926ce68011bc67a180d16e0d36a18fd77301
Gold856 <117957790+Gold856@users.noreply.github.com>
2025-02-04 13:43:42 -0500

    Sort constants, do general code clean up, rename elevator commands

--- src/main/java/frc/robot/subsystems/CheeseStickSubsystem.java
+++ src/main/java/frc/robot/subsystems/CheeseStickSubsystem.java
@@ -3,40 +3,39 @@ package frc.robot.subsystems;
 import static frc.robot.Constants.CheeseStickConstants.*;
 
 import edu.wpi.first.wpilibj.Servo;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class CheeseStickSubsystem extends SubsystemBase {
-	private Servo m_motor;
-	public Timer time;
+	private final Servo m_servo = new Servo(kServoPort);
 
 	public CheeseStickSubsystem() {
-		m_motor = new Servo(kServoPort);
-		time = new Timer();
-		m_motor.setBoundsMicroseconds(2500, 0, 1500, 0, 500);
-	}
-
-	public void init() {
-		m_motor.setZeroLatch();
+		// https://docs.revrobotics.com/rev-crossover-products/servo/srs#electrical-specifications
+		m_servo.setBoundsMicroseconds(2500, 0, 0, 0, 500);
+		m_servo.setZeroLatch();
 	}
 
 	@Override
 	public void periodic() {
-		SmartDashboard.putNumber("Time", time.get());
-		SmartDashboard.putNumber("Position", m_motor.getPosition());
 	}
 
 	/**
-	 * Rotates the servo. Does not wait for it to finish.
+	 * Returns a command to command the servo to rotate to the left. Does not wait
+	 * for it to finish.
 	 * 
-	 * @param angle The angle from 0 in the left to 270 in the right.
 	 * @return The command.
 	 */
-	public Command rotateCommand(Double angle) {
-		if (angle == null)
-			return runOnce(() -> m_motor.set(.5));
-		return runOnce(() -> m_motor.set(angle / kMaxRotation)).withName("Rotate Servo");
+	public Command goLeft() {
+		return runOnce(() -> m_servo.set(0)).withName("Servo Go Left");
+	}
+
+	/**
+	 * Returns a command to command the servo to rotate to the right. Does not wait
+	 * for it to finish.
+	 * 
+	 * @return The command.
+	 */
+	public Command goRight() {
+		return runOnce(() -> m_servo.set(1)).withName("Servo Go Right");
 	}
 }
