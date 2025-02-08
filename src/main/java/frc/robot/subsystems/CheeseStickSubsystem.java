9cb93668850a5d7a491079619de04ac248573c10
JonahEMorgan <149741383+JonahEMorgan@users.noreply.github.com>
2025-02-08 15:22:55 -0500

    Update Cheese Stick Subsystem

--- src/main/java/frc/robot/subsystems/CheeseStickSubsystem.java
+++ src/main/java/frc/robot/subsystems/CheeseStickSubsystem.java
@@ -1,41 +1,56 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.*;
 import static frc.robot.Constants.CheeseStickConstants.*;
 
 import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.simulation.PWMSim;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismObject2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class CheeseStickSubsystem extends SubsystemBase {
 	private final Servo m_servo = new Servo(kServoPort);
+	private final PWMSim m_sim = new PWMSim(m_servo);
+	private final MechanismLigament2d m_ligament = new MechanismLigament2d("Cheeese Stick", 0, 90);
 
-	public CheeseStickSubsystem() {
+	public CheeseStickSubsystem(MechanismObject2d attachment) {
 		// https://docs.revrobotics.com/rev-crossover-products/servo/srs#electrical-specifications
 		m_servo.setBoundsMicroseconds(2500, 0, 0, 0, 500);
 		m_servo.setZeroLatch();
+		attachment.append(m_ligament);
 	}
 
-	@Override
+	/**
+	 * Logs data to the smart dashboard. Displays the simulated mechanism.
+	 */
 	public void periodic() {
+		double position = m_sim.getPosition();
+		SmartDashboard.putNumber("Cheese Stick Position", position);
+		m_ligament.setLength(kExtensionLength.in(Meters) * position);
 	}
 
 	/**
-	 * Returns a command to command the servo to rotate to the left. Does not wait
+	 * Returns a command to command the servo to rotate anti-clockwise. This pulls
+	 * against springs and retracts the cheese stick. Does not wait
 	 * for it to finish.
 	 * 
 	 * @return The command.
 	 */
-	public Command goLeft() {
-		return runOnce(() -> m_servo.set(0)).withName("Servo Go Left");
+	public Command retract() {
+		return runOnce(() -> m_servo.set(0)).withName("Servo Retract");
 	}
 
 	/**
-	 * Returns a command to command the servo to rotate to the right. Does not wait
+	 * Returns a command to command the servo to rotate clockwise. This is pushed by
+	 * springs and extends the cheese stock. Does not wait
 	 * for it to finish.
 	 * 
 	 * @return The command.
 	 */
-	public Command goRight() {
-		return runOnce(() -> m_servo.set(1)).withName("Servo Go Right");
+	public Command extend() {
+		return runOnce(() -> m_servo.set(1)).withName("Servo Extend");
 	}
 }
