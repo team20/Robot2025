75a275257b4bce66bfc923c8b0a118939e2edbf2
J Hwang <100035934+jhhbrown1@users.noreply.github.com>
2025-03-07 22:34:43 -0500

    position-based completion of ElevatorSubystem#goToLevel(DoubleSupplier)

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -220,7 +220,8 @@ public class ElevatorSubsystem extends SubsystemBase {
 			SmartDashboard.putNumber("Elevator/Next Target Velocity", nextState.velocity);
 			SmartDashboard.putNumber("Elevator/Profile Time", m_profile.totalTime());
 			SmartDashboard.putNumber("Elevator/Current Time", m_timer.get());
-		}).until(() -> m_profile.isFinished(m_timer.get()));
+		}).until(() -> Math.abs(finalState.position - getPosition()) <= kTolerance);
+		// }).until(() -> m_profile.isFinished(m_timer.get()));
 	}
 
 	/**
