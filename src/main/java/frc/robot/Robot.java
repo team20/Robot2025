e5773e21eb2cedbd9254def711d61915278a2c7d
Team20DriverStation <159724770+Team20DriverStation@users.noreply.github.com>
2025-03-13 17:47:17 -0400

    Got rid of USB Camera

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -28,14 +28,11 @@ import org.photonvision.simulation.SimCameraProperties;
 
 import com.ctre.phoenix6.SignalLogger;
 
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.net.WebServer;
-import edu.wpi.first.util.PixelFormat;
 import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj.DataLogManager;
@@ -105,10 +102,10 @@ public class Robot extends TimedRobot {
 	};
 	private final PhotonCamera m_camera1 = RobotBase.isSimulation()
 			? cameraSim("Camera1", kRobotToCamera1, m_visionSimulator, cameraProp)
-			: new PhotonCamera("FrontCamera");
+			: new PhotonCamera("BackCamera");
 	private final PhotonCamera m_camera2 = RobotBase.isSimulation()
 			? cameraSim("Camera2", kRobotToCamera2, m_visionSimulator, cameraProp)
-			: new PhotonCamera("BackCamera");
+			: new PhotonCamera("FrontCamera");
 	private final PoseEstimationSubsystem m_poseEstimationSubsystem = new PoseEstimationSubsystem(m_driveSubsystem)
 			.addCamera(m_camera1, kRobotToCamera1)
 			.addCamera(m_camera2, kRobotToCamera2);
@@ -152,10 +149,6 @@ public class Robot extends TimedRobot {
 		SmartDashboard.putData("Auto Selector", m_autoSelector);
 		m_driverController.options().and(m_driverController.create()).and(() -> !DriverStation.isFMSAttached())
 				.onTrue(Commands.deferredProxy(m_testingChooser::getSelected));
-		if (RobotBase.isReal()) {
-			UsbCamera camera = CameraServer.startAutomaticCapture();
-			camera.setVideoMode(PixelFormat.kMJPEG, 160, 120, 30);
-		}
 	}
 
 	public void addAutoCommands() {
@@ -163,6 +156,7 @@ public class Robot extends TimedRobot {
 				.addOption(
 						"Middle and Algae Blue", CommandComposer.getMiddleScoreAndAlgaeBlue());
 		m_autoSelector.addOption("Leave", CommandComposer.leave());
+		m_autoSelector.addOption("Middle and Algae Red", CommandComposer.getMiddleScoreAndAlgaeRed());
 	}
 
 	public void addTestingCommands() {
