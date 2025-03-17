3ebca373e5d5b89faf38994bdc7bdf2f485550be
J Hwang <100035934+jhhbrown1@users.noreply.github.com>
2025-03-16 23:59:57 -0400

    kTolerance

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -57,6 +57,7 @@ public class Constants {
 		public static final double kP = 0.75;
 		public static final double kI = 0;
 		public static final double kD = 0;
+		public static final double kTolerance = 1;
 		public static final int kSmartCurrentLimit = 50;
 		public static final int kSecondaryCurrentLimit = kSmartCurrentLimit + 15;
 	}
